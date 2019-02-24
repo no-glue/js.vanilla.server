2c5d9f94354884a41e33e0dfda56d26ee99f0171
nikolapav1985 <nikolapav1985@gmail.com>
2019-02-25 00:44:06 +0100

    index

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express=require('express');
 var app=express();
 var port=3001;
+var dirty=require('dirty');
+var db = dirty('user.db');
 
-app.get('/', function(req,res){
+db.on('load',function{
+    db.set('john',{likes:5});
+    db.set('jane',{likes:7});
+    db.set('abe',{likes:11});
+    db.set('martha',{likes:17});
+    db.set('lisa',{likes:23});
+    db.set('anthony',{likes:1});
+    db.set('mark',{likes:2});
+    db.set('beth',{likes:3});
+    db.set('jill',{likes:29});
+    db.set('bob',{likes:31});
+    db.set('xavier',{likes:37});
+});
+
+app.get('/users', function(req,res){
     res.send('hello');
 });
 app.listen(port,function(){
